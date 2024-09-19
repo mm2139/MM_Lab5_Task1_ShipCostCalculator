fa07aa1c9c48d9c6d3198c2e402fa5c47ab976e2
mmaalin <maalin.622139@swl.k12.oh.us>
2024-09-19 08:34:22 -0400

    finished lab

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,5 +1,29 @@
-public class Main {
+import java.util.Scanner;
+
+public class ShipCostCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        double cost;
+        double shipping;
+
+        System.out.println("What is the price of your item?");
+
+        if (scan.hasNextDouble()) {
+            cost = scan.nextDouble();
+            if (cost < 100) {
+                shipping = cost*0.2;
+                cost += shipping;
+                System.out.println("Shipping is 2% of your item price, which will be " + shipping + ". The total cost of your order is " + cost + ".");
+            }
+            else {
+                System.out.println("Shipping is free. The total cost of your order is " + cost + ".");
+            }
+        }
+        else {
+            System.out.println("You have not inputed a valid price.");
+            System.exit(0);
+        }
+
     }
 }
